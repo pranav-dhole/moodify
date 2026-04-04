bcd79cbbc32ecacda6d626dbcd9068c95501386f
Pranav <bizzareforyou@gmail.com>
2026-04-04 17:48:11 +0530

    created to send youtube song details to playlist component

---
+++ src/hooks/getYoutubeData.js
@@ -0,0 +1,15 @@
+const youtubeKey = import.meta.env.VITE_YOUTUBE_API_KEY;
+
+export const getYouTubeData = async (trackName, artistName) => {
+  const query = encodeURIComponent(`${trackName} ${artistName} official audio`);
+  const res = await fetch(
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${youtubeKey}&maxResults=1&type=video`,
+  );
+  const data = await res.json();
+  const item = data.items[0];
+
+  return {
+    id: item?.id?.videoId,
+    thumb: item?.snippet?.thumbnails?.default?.url,
+  };
+};
