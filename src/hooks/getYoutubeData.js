06e19d1d51a7c390fb95f16de3d61401445da8da
Pranav <bizzareforyou@gmail.com>
2026-04-05 15:22:33 +0530

    added try catch error handling

--- src/hooks/getYoutubeData.js
+++ src/hooks/getYoutubeData.js
@@ -1,15 +1,23 @@
 const youtubeKey = import.meta.env.VITE_YOUTUBE_API_KEY;
 
 export const getYouTubeData = async (trackName, artistName) => {
-  const query = encodeURIComponent(`${trackName} ${artistName} official audio`);
-  const res = await fetch(
-    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${youtubeKey}&maxResults=1&type=video`,
-  );
-  const data = await res.json();
-  const item = data.items[0];
+  try {
+    console.log("youtube");
 
-  return {
-    id: item?.id?.videoId,
-    thumb: item?.snippet?.thumbnails?.default?.url,
-  };
+    const query = encodeURIComponent(
+      `${trackName} ${artistName} official audio`,
+    );
+    const res = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${youtubeKey}&maxResults=1&type=video`,
+    );
+    const data = await res.json();
+    const item = data.items[0];
+
+    return {
+      id: item?.id?.videoId,
+      thumb: item?.snippet?.thumbnails?.default?.url,
+    };
+  } catch (err) {
+    console.error(err.message);
+  }
 };
